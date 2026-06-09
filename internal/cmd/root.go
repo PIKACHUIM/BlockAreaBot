6a76efef17a1e0a8bb143a8397bd8a5b926f21ef
pikachuim <pikachuim@qq.com>
2026-06-09 17:04:24 +0800

    add(all): basic finish coding

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -2,8 +2,13 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
+
+// Version 版本号，通过 ldflags 注入
+var Version = "dev"
 
 var rootCmd = &cobra.Command{
 	Use:   "block",
@@ -11,6 +16,7 @@ var rootCmd = &cobra.Command{
 	Long: `Block Area Bot 是一个 Linux 服务 + CLI 工具，
 用于基于地区、IP段、ASN 来屏蔽特定来源对服务器的访问。
 底层使用 iptables + ipset 实现高效的 IP 段屏蔽。`,
+	Version: Version,
 }
 
 // Execute 执行根命令
@@ -18,7 +24,14 @@ func Execute() error {
 	return rootCmd.Execute()
 }
 
+// SetVersion 设置版本号（由 main 包调用）
+func SetVersion(v string) {
+	Version = v
+	rootCmd.Version = v
+}
+
 func init() {
+	rootCmd.SetVersionTemplate(fmt.Sprintf("Block Area Bot %s\n", Version))
 	// 添加子命令组
 	rootCmd.AddCommand(newStartCmd())
 	rootCmd.AddCommand(newStopCmd())
@@ -30,4 +43,5 @@ func init() {
 	rootCmd.AddCommand(newRuleCmd())
 	rootCmd.AddCommand(newCronCmd())
 	rootCmd.AddCommand(newDaemonCmd())
+	rootCmd.AddCommand(newUpdateCmd())
 }
--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 	"github.com/soulteary/block-area-bot/internal/cmd"
 )
 
+// version 通过 ldflags 注入
+var version = "dev"
+
 func main() {
+	cmd.SetVersion(version)
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "错误: %v\n", err)
 		os.Exit(1)
